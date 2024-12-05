121dab2b1e9dcd37db1ed2f17c4ab767b8bc2fc6
HieuCoiDev <hsrnguyenkai@gmail.com>
2024-12-05 13:25:42 +0700

    Add files via upload

---
+++ csdlDeMo.sql
@@ -0,0 +1,152 @@
+﻿
+CREATE DATABASE QLTV;
+GO
+
+USE QLTV;
+GO
+
+CREATE TABLE admin (
+    idAdmin INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+    username NVARCHAR(20) NOT NULL,
+    adPass NVARCHAR(50) NOT NULL
+);
+GO
+
+CREATE TABLE dangNhap (
+    maDangNhap NVARCHAR(10) PRIMARY KEY NOT NULL,
+    nvPass NVARCHAR(20) NOT NULL
+);
+GO
+
+CREATE TABLE Sach (
+    idBook INT IDENTITY(1,1) NOT NULL,
+    maSach NVARCHAR(10) NOT NULL,
+    tenSach NVARCHAR(150),
+    tacGia NVARCHAR(100),
+    nhaXuatBan NVARCHAR(100),
+    namXuatBan DATE,
+    theLoai NVARCHAR(50),
+    viTriTV NVARCHAR(100),
+    soLuong INT NOT NULL,
+    CONSTRAINT PK_Sach PRIMARY KEY (idBook), -- Khóa chính cho cột idBook
+    CONSTRAINT UQ_maSach UNIQUE (maSach) -- Ràng buộc duy nhất cho cột maSach
+);
+GO
+
+CREATE TABLE docGia (
+    idDocGia NVARCHAR(10) PRIMARY KEY NOT NULL,
+    tenDocGia NVARCHAR(100),
+    diaChi NVARCHAR(150),
+    sdtDocGia NVARCHAR(10),
+    emailDocGia TEXT
+);
+GO
+
+CREATE TABLE nhanVien (
+    idNhanVien NVARCHAR(10) PRIMARY KEY NOT NULL,
+    tenNhanVien NVARCHAR(100),
+    chucVu NVARCHAR(50),
+    sdtNhanVien NVARCHAR(10),
+    khuVuc NVARCHAR(20),
+    emailNhanVien TEXT,
+    diaChiNhanVien NVARCHAR(150)
+);
+GO
+
+CREATE TABLE suKien (
+    idSuKien NVARCHAR(10) PRIMARY KEY NOT NULL,
+    tenSuKien NVARCHAR(100),
+    moTaSuKien NVARCHAR(200),
+    ngayToChuc DATE,
+    gioToChuc NVARCHAR(50),
+    diaDiemToChuc NVARCHAR(200)
+);
+GO
+
+CREATE TABLE coSoVatChat (
+    idThietBi NVARCHAR(10) PRIMARY KEY NOT NULL,
+    tenThietBi NVARCHAR(50),
+    tinhTrangThietBi NVARCHAR(100),
+    ngayMua DATE,
+    ngayBaoTri DATE
+);
+GO
+
+CREATE TABLE Kho (
+    maSach NVARCHAR(10) NOT NULL,
+    soLuong INT NOT NULL,
+    trangThai NVARCHAR(50),
+);
+GO
+
+ALTER TABLE Kho
+ADD CONSTRAINT FK_Kho_Sach FOREIGN KEY (maSach) REFERENCES Sach(maSach);
+
+CREATE TABLE phieuMuon (
+    idPhieuMuon NVARCHAR(10) PRIMARY KEY NOT NULL,
+    maSach NVARCHAR(10),
+    idDocGia NVARCHAR(10),
+    idNhanVien NVARCHAR(10),
+    soLuong INT NOT NULL,
+    ngayLapPhieu DATE,
+    ngayHenTra DATE,
+    FOREIGN KEY (maSach) REFERENCES Sach(maSach),
+    FOREIGN KEY (idDocGia) REFERENCES docGia(idDocGia),
+    FOREIGN KEY (idNhanVien) REFERENCES nhanVien(idNhanVien)
+);
+GO
+
+CREATE TABLE thongKe (
+    idNhanVien NVARCHAR(10) NOT NULL,
+    idDocGia NVARCHAR(10) NOT NULL,
+    idSach NVARCHAR(10) NOT NULL,
+    ngayThongKe DATE,
+    soLanMuon INT,
+    soLanTra INT,
+    PRIMARY KEY (idNhanVien, idDocGia, idSach),
+    FOREIGN KEY (idNhanVien) REFERENCES nhanVien(idNhanVien),
+    FOREIGN KEY (idSach) REFERENCES Sach(maSach),
+    FOREIGN KEY (idDocGia) REFERENCES docGia(idDocGia)
+);
+GO
+
+CREATE TABLE phieuTra (
+    idPhieuMuon NVARCHAR(10),
+    idNhanVien NVARCHAR(10),
+    tenNhanVien1 NVARCHAR(100),
+    tenDocGia NVARCHAR(100),
+    trangThai NVARCHAR(50),
+    ngayTraThuc DATE,
+    ngayHenTra DATE,
+    phiTreHen FLOAT,
+    phiPhatSinh FLOAT,
+    soLuong INT,
+    PRIMARY KEY (idPhieuMuon),
+    FOREIGN KEY (idPhieuMuon) REFERENCES phieuMuon(idPhieuMuon),
+    FOREIGN KEY (idNhanVien) REFERENCES nhanVien(idNhanVien)
+);
+GO
+
+CREATE TABLE Luong (
+    idNhanVien NVARCHAR(10),
+    ngayDiLam DATE,
+    thuong MONEY,
+    luong MONEY,
+    chucVu NVARCHAR(50),
+    PRIMARY KEY (idNhanVien),
+    FOREIGN KEY (idNhanVien) REFERENCES nhanVien(idNhanVien)
+);
+GO
+
+CREATE TABLE chamCong (
+    idNhanVien NVARCHAR(10),
+    chucVu NVARCHAR(50),
+    ngayDiLam DATE,
+    tinhTrang NVARCHAR(100),
+    phep BIT,
+    PRIMARY KEY (idNhanVien),
+    FOREIGN KEY (idNhanVien) REFERENCES nhanVien(idNhanVien)
+);
+GO
+
+ insert into admin (username, adPass) values ('admin','08072003')
---
+++ sqlTest.sql
@@ -0,0 +1,50 @@
+﻿select ngayLapPhieu, count(*) as Số_lần_mượn_trong_ngày
+from phieuMuon
+group by ngayLapPhieu
+
+select month(ngayLapPhieu) as tháng, count(*) as Số_lần_mượn_trong_tháng
+from phieuMuon
+group by month(ngayLapPhieu)
+
+select dg.tenDocGia, month(pm.ngayLapPhieu) as Tháng, sum(pm.soLuong) as SoLuongSachMuon from phieuMuon pm, docGia dg where pm.idDocGia = dg.idDocGia group by dg.tenDocGia, month(pm.ngayLapPhieu)
+
+select nv.tenNhanVien as Tên_Nhân_Viên, pm.ngayLapPhieu as Ngày ,count(*) as Lập_phiếu
+from nhanVien nv, phieuMuon pm
+where nv.idNhanVien = pm.idNhanVien
+group by nv.tenNhanVien, pm.ngayLapPhieu
+
+select nv.tenNhanVien as Tên_Nhân_Viên, month(pm.ngayLapPhieu) as Tháng ,count(*) as Lập_phiếu
+from nhanVien nv, phieuMuon pm
+where nv.idNhanVien = pm.idNhanVien
+group by nv.tenNhanVien, month(pm.ngayLapPhieu)
+
+select s.tenSach as Tên_sách, pm.ngayLapPhieu as Ngày, count(*) as Lần
+from Sach s, phieuMuon pm
+where s.idSach = pm.idSach
+group by s.tenSach, pm.ngayLapPhieu
+
+select s.tenSach as Tên_sách, month(pm.ngayLapPhieu) as Tháng, count(*) as Lần
+from Sach s, phieuMuon pm
+where s.idSach = pm.idSach
+group by s.tenSach, month(pm.ngayLapPhieu)
+
+select s.tenSach as Tên_sách, count(*) as Lần
+from Sach s, phieuMuon pm
+where s.idSach = pm.idSach
+group by s.tenSach
+
+select nv.tenNhanVien as Tên_Nhân_Viên, pt.ngayTraThuc as Ngày ,count(*) as Lập_phiếu_trả
+from nhanVien nv, phieuTra pt, phieuMuon pm
+where nv.idNhanVien = pt.idNhanVien and pt.idPhieuMuon = pm.idPhieuMuon
+group by nv.tenNhanVien, pt.ngayTraThuc
+
+select nv.tenNhanVien as Tên_Nhân_Viên, month(pt.ngayTraThuc) as Tháng ,count(*) as Lập_phiếu_trả
+from nhanVien nv, phieuTra pt, phieuMuon pm
+where nv.idNhanVien = pt.idNhanVien and pt.idPhieuMuon = pm.idPhieuMuon
+group by nv.tenNhanVien, month(pt.ngayTraThuc)
+
+select * 
+from phieuMuon pm
+
+select * 
+from phieuTra pt
